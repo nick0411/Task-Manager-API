4130b4dde8e0ab2f9a381543cda76d301740ff82
nick <nlh041120@gmail.com>
2025-06-20 01:31:25 +0630

    Initial commit: Spring Boot Task Manager API

---
+++ src/test/java/com/example/Task_Manager_API/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Task_Manager_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
